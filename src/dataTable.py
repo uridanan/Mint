4ee2c92deb0bd0ae134cda073a2500e6152923f1
uridanan <uridanan@gmail.com>
2018-11-12 00:11:03 +0200

    dataTable with my own data
    TODO: get updated row in callback instead of the whole table

--- src/dataTable.py
+++ src/dataTable.py
@@ -4,35 +4,69 @@ import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
+import src.dbAccess as db
 
 app = dash.Dash(__name__)
 
-params = [
-    'Weight', 'Torque', 'Width', 'Height',
-    'Efficiency', 'Power', 'Displacement'
-]
-
-app.layout = html.Div([
-    dash_table.DataTable(
-        id='table-editing-simple',
+#=============================================================================================================
+def generateTable(dataframe, max_rows=200):
+    return dash_table.DataTable(
+        id='monthly-report-table',
+        # Header
         columns=(
-            [{'id': 'Model', 'name': 'Model'}] +
-            [{'id': p, 'name': p} for p in params]
+            [{'id': p, 'name': p} for p in dataframe.columns]
         ),
+        # Body
         data=[
-            dict(Model=i, **{param: 0 for param in params})
-            for i in range(1, 5)
+            dict(entry=i,**{col: dataframe.iloc[i][col] for col in dataframe.columns})
+            for i in range(min(len(dataframe), max_rows))
         ],
         editable=True
-    ),
-    dcc.Graph(id='table-editing-simple-output')
+    )
+
+F_MONTHLY = 'src/queryMonthlyReport.sql'
+reportData = db.runQueryFromFile(F_MONTHLY)
+
+app.layout = html.Div(children=[
+    html.H4(children='Editable Expense Report - Work In Pogress'),
+    generateTable(reportData),
+    html.H5(id='output')
 ])
 
+#=============================================================================================================
+
+
+
+# params = [
+#     'Weight', 'Torque', 'Width', 'Height',
+#     'Efficiency', 'Power', 'Displacement'
+# ]
+
+# app.layout = html.Div([
+#     dash_table.DataTable(
+#         id='table-editing-simple',
+#         columns=(
+#             [{'id': 'Model', 'name': 'Model'}] +
+#             [{'id': p, 'name': p} for p in params]
+#         ),
+#         data=[
+#             dict(Model=i, **{param: 0 for param in params})
+#             for i in range(1, 5)
+#         ],
+#         editable=True
+#     ),
+#     dcc.Graph(id='table-editing-simple-output')
+# ])
+
+
+#TODO: save updated data
+#TODO: format the table
+#https://community.plot.ly/t/solved-updating-a-dash-datatable-rows-with-row-update-and-rows/6573/2
 
 @app.callback(
-    Output('table-editing-simple-output', 'figure'),
-    [Input('table-editing-simple', 'data'),
-     Input('table-editing-simple', 'columns')])
+    Output('output', 'contentEditable'),
+    [Input('monthly-report-table', 'row_update'),
+     Input('monthly-report-table', 'columns')])
 
 def display_output(rows, columns):
     df = pd.DataFrame(rows, columns=[c['name'] for c in columns])
