e61e5f4f44fbb648e70c3bc0d892aaf037ade5e7
uridanan <uridanan@gmail.com>
2018-11-13 00:18:08 +0200

    Create editable table
    Option 1 : dataTable
    Option 2: dcc.input

--- src/dataTable.py
+++ src/dataTable.py
@@ -1,10 +1,11 @@
 import dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import src.dbAccess as db
+import copy
 
 app = dash.Dash(__name__)
 
@@ -36,40 +37,16 @@ app.layout = html.Div(children=[
 #=============================================================================================================
 
 
-
-# params = [
-#     'Weight', 'Torque', 'Width', 'Height',
-#     'Efficiency', 'Power', 'Displacement'
-# ]
-
-# app.layout = html.Div([
-#     dash_table.DataTable(
-#         id='table-editing-simple',
-#         columns=(
-#             [{'id': 'Model', 'name': 'Model'}] +
-#             [{'id': p, 'name': p} for p in params]
-#         ),
-#         data=[
-#             dict(Model=i, **{param: 0 for param in params})
-#             for i in range(1, 5)
-#         ],
-#         editable=True
-#     ),
-#     dcc.Graph(id='table-editing-simple-output')
-# ])
-
-
 #TODO: save updated data
 #TODO: format the table
 #https://community.plot.ly/t/solved-updating-a-dash-datatable-rows-with-row-update-and-rows/6573/2
 
 @app.callback(
-    Output('output', 'contentEditable'),
-    [Input('monthly-report-table', 'row_update'),
-     Input('monthly-report-table', 'columns')])
-
-def display_output(rows, columns):
-    df = pd.DataFrame(rows, columns=[c['name'] for c in columns])
+    Output('monthly-report-table', 'data'),
+    [Input('monthly-report-table', 'updated_cells')],
+    [State('monthly-report-table', 'data')])
+def display_output(data, columns):
+    df = pd.DataFrame(data, columns=[c['name'] for c in columns])
     return {
         'data': [{
             'type': 'parcoords',
@@ -79,6 +56,24 @@ def display_output(rows, columns):
             } for col in columns]
         }]
     }
+
+
+
+
+# @app.callback(
+#     Output('editable-table', 'rows'),
+#     [Input('editable-table', 'row_update')],
+#     [State('editable-table', 'rows')])
+# def update_rows(row_update, rows):
+#     row_copy = copy.deepcopy(rows)
+#     if row_update:
+#         updated_row_index = row_update[0]['from_row']
+#         updated_value = row_update[0]['updated'].values()[0]
+#         row_copy[updated_row_index]['Output'] = (
+#             float(updated_value) ** 2
+#         )
+#     return row_copy
+
 
 
 if __name__ == '__main__':
--- src/monthlyReport.py
+++ src/monthlyReport.py
@@ -2,6 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import src.dbAccess as db
+from dash.dependencies import Input, Output, State
 
 #Am I better off getting the data via REST API or directly from DB?
 #It seems more direct to use the DB, I see no need for the overhead of APIs just yet
@@ -16,12 +17,35 @@ def generateTable(dataframe, max_rows=200):
 
         # Body
         [html.Tr([
-            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            html.Td(editableCell(dataframe.iloc[i][col],i,col)) for col in dataframe.columns
         ]) for i in range(min(len(dataframe), max_rows))]
     )
 
-def generateTablePandas(dataFrame):
-    return dataFrame.describe().to_html()
+#TODO: handle onclick callback to save data
+#TODO: style edit box to make it invisible
+#TODO: use only for editable cells, not all
+#TODO: update on enter, not every keypress, why does debounce not work?
+#TODO: join with category
+def editableCell(text,row,col):
+    strId = '.'.join([str(row),str(col)])
+    cell = dcc.Input(id = strId, type = 'text', value = text, disabled=False) #setProps,debounce
+    return cell
+
+@app.callback(
+    Output(component_id='output', component_property='children'),
+    [Input(component_id='my-id', component_property='id')],
+    [Input(component_id='my-id', component_property='value')]
+)
+def update_output_div(id,value):
+    return 'You\'ve entered "{}"'.format(value)
+
+
+
+def onUpdate(id):
+    print(id)
+
+# def generateTablePandas(dataFrame):
+#     return dataFrame.describe().to_html()
 
 
 def generateBarGraph(data, xName, yNames, names):
@@ -54,7 +78,8 @@ app.layout = html.Div(children=[
  #   dcc.Graph(id='balance-graph',figure=generateBarGraph(balanceData,"monthname",["balance"],["Balance"])),
  #   dcc.Graph(id='savings-graph',figure=generateBarGraph(savingsData,"monthname",["savings"],["Savings"])),
  #   dcc.Graph(id='income-graph',figure=generateBarGraph(savingsData,"monthname",["monthlycredit","monthlydebit"],["Income","Expenses"])),
-    generateTable(reportData)
+    generateTable(reportData),
+    html.Div(id='output')
 ])
 
 
