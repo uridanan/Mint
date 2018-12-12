c1572dd3496191a975bda43725294537669d675a
uridanan <uridanan@gmail.com>
2018-12-12 23:39:26 +0200

    Categories over time - WIP
    Works but needs formatting

--- src/display.py
+++ src/display.py
@@ -3,6 +3,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 import src.dbAccess as db
 from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.graph_objs as go
 
 from flask import send_from_directory
 import os
@@ -21,6 +23,7 @@ import os
 #https://plot.ly/python/time-series/
 #TODO Look at morning star tickers for proper time series?
 #https://dash.plot.ly/gallery
+#https://dash.plot.ly/dash-core-components/tabs
 
 #TODO: Format fields per data type
 #TODO: Make the table scrollable (use my own CSS)
@@ -32,12 +35,157 @@ import os
 #TODO: manage categories form: create categories and assign businesses to them
 #TODO: think of suggesting categories based on classification by others, but each uesr gets to assign businesses as they like
 
-#TODO: Use tabs to select which graph to display https://dash.plot.ly/dash-core-components/tabs
+
 #TODO: expenses by category over time
+#TODO: add range slider to select dates range
 #TODO: start marking recurring expenses
 #TODO: import the rest of the credit cards
 #TODO: rename expense (save the new name, re-use when recurring)
 #TODO: show credit (income) report
+#TODO: use upload component to upload files
+
+
+
+class TimeSeriesData:
+    data = dict()
+    dates = []
+    range = []
+
+    def __init__(self,dataFrame):
+        self.data = self.groupByDate(dataFrame)
+
+    def groupByDate(self,dataFrame):
+        data = dict()
+        start = None
+        end = None
+        for row in dataFrame.values:
+            date = row[0]
+            key = row[1]
+            value = row[2]
+            self.dates.append(date)
+            if start == None:
+                start = date
+            end = date
+            if date in data:
+                entry = data[date]
+            else:
+                entry = dict()
+            entry[key] = value
+            data[date] = entry
+        self.range = [start,end]
+        return data
+
+    def getSeriesByName(self,name):
+        values = []
+        for k in self.data.keys():
+            v = 0
+            if name in self.data[k].keys() and self.data[k][name] != None:
+                v = self.data[k][name]
+            values.append(v)
+        return values
+
+    def getDates(self):
+        return self.dates
+
+    def getRange(self):
+        return self.range
+
+#Format using the example "Label Lines with Annotations" from https://plot.ly/python/line-charts/
+def generateTimeSeries(categories,dataFrame):
+    #df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv")
+    timeSeries = TimeSeriesData(dataFrame)
+    data = []
+    for c in categories:
+        trace = go.Scatter(
+            x=timeSeries.getDates(),
+            y=timeSeries.getSeriesByName(c),
+            name = c,
+            line = dict(color = '#17BECF'),
+            opacity = 0.8)
+        data.append(trace)
+    layout = dict(
+        title = "Expenses by category over time",
+        xaxis = dict(title='Month'),
+        yaxis=dict(title='Expenses')
+    )
+    figure = dict(data=data, layout=layout)
+    return figure
+
+
+def testFigure():
+    # Add data
+    month = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+             'August', 'September', 'October', 'November', 'December']
+    high_2000 = [32.5, 37.6, 49.9, 53.0, 69.1, 75.4, 76.5, 76.6, 70.7, 60.6, 45.1, 29.3]
+    low_2000 = [13.8, 22.3, 32.5, 37.2, 49.9, 56.1, 57.7, 58.3, 51.2, 42.8, 31.6, 15.9]
+    high_2007 = [36.5, 26.6, 43.6, 52.3, 71.5, 81.4, 80.5, 82.2, 76.0, 67.3, 46.1, 35.0]
+    low_2007 = [23.6, 14.0, 27.0, 36.8, 47.6, 57.7, 58.9, 61.2, 53.3, 48.5, 31.0, 23.6]
+    high_2014 = [28.8, 28.5, 37.0, 56.8, 69.7, 79.7, 78.5, 77.8, 74.1, 62.6, 45.3, 39.9]
+    low_2014 = [12.7, 14.3, 18.6, 35.5, 49.9, 58.0, 60.0, 58.6, 51.7, 45.2, 32.2, 29.1]
+
+    # Create and style traces
+    trace0 = go.Scatter(
+        x=month,
+        y=high_2014,
+        name='High 2014',
+        line=dict(
+            color=('rgb(205, 12, 24)'),
+            width=4)
+    )
+    trace1 = go.Scatter(
+        x=month,
+        y=low_2014,
+        name='Low 2014',
+        line=dict(
+            color=('rgb(22, 96, 167)'),
+            width=4, )
+    )
+    trace2 = go.Scatter(
+        x=month,
+        y=high_2007,
+        name='High 2007',
+        line=dict(
+            color=('rgb(205, 12, 24)'),
+            width=4,
+            dash='dash')  # dash options include 'dash', 'dot', and 'dashdot'
+    )
+    trace3 = go.Scatter(
+        x=month,
+        y=low_2007,
+        name='Low 2007',
+        line=dict(
+            color=('rgb(22, 96, 167)'),
+            width=4,
+            dash='dash')
+    )
+    trace4 = go.Scatter(
+        x=month,
+        y=high_2000,
+        name='High 2000',
+        line=dict(
+            color=('rgb(205, 12, 24)'),
+            width=4,
+            dash='dot')
+    )
+    trace5 = go.Scatter(
+        x=month,
+        y=low_2000,
+        name='Low 2000',
+        line=dict(
+            color=('rgb(22, 96, 167)'),
+            width=4,
+            dash='dot')
+    )
+    data = [trace0, trace1, trace2, trace3, trace4, trace5]
+
+    # Edit the layout
+    layout = dict(title='Average High and Low Temperatures in New York',
+                  xaxis=dict(title='Month'),
+                  yaxis=dict(title='Temperature (degrees F)'),
+                  )
+
+    fig = dict(data=data, layout=layout)
+    return fig
 
 #Try an iplot instead
 #https://plot.ly/python/table/
@@ -64,9 +212,21 @@ def generateBarGraph(data, xName, yNames, names):
 
 F_BALANCE = 'src/queryBalanceReport.sql'
 F_SAVINGS = 'src/querySavingsReport.sql'
+F_GETCATEGORIES = 'src/queryCategoryFilter.sql'
+F_CATEGORIESOVERTIME = 'src/queryExpensesByCategoryOverTime.sql'
+
+def getCategories():
+    df = db.runQueryFromFile(F_GETCATEGORIES)
+    list = [v[0] for v in df.values]
+    return list
 
 balanceData = db.runQueryFromFile(F_BALANCE)
 savingsData = db.runQueryFromFile(F_SAVINGS)
+categories = getCategories()
+categoriesData = db.runQueryFromFile(F_CATEGORIESOVERTIME)
+
+testFigure = testFigure()
+myFigure = generateTimeSeries(categories,categoriesData)
 
 #app = dash.Dash()
 app = dash.Dash(__name__)
@@ -82,7 +242,8 @@ app.layout = html.Div(children=[
         dcc.Tab(label='Savings', value='tab2'),
         dcc.Tab(label='IncomeVSExpenses', value='tab3')
     ]),
-    html.Div(id='tabsContent')
+    html.Div(id='tabsContent'),
+    dcc.Graph(id='byCategory',figure=generateTimeSeries(categories,categoriesData))
 ])
 
 
---
+++ src/queryExpensesByCategoryOverTime.sql
@@ -0,0 +1,18 @@
+select
+to_char(to_date(month,'YYYY-MM'),'Mon YYYY') as date,
+business_entry.category,
+SUM(U.debit) as amount
+from (
+select to_char(report_date, 'YYYY-MM') as month, debit, business
+from credit_entry
+union
+select to_char(date, 'YYYY-MM') as month, debit, business
+from bank_entry
+where hide = 0
+)
+as U
+inner join business_entry on business_entry.id = U.business
+where debit > 0
+and business_name <> '__TOTAL__'
+group by category, month
+order by month,category asc
