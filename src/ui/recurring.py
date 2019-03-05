4fa9cd7d43ca662db53212be31d854bd9f1ede6b
uridanan <uridanan@gmail.com>
2019-03-06 00:31:40 +0200

    Recurring Expenses - WIP
    Add slider and apply to graph - DONE

--- src/ui/recurring.py
+++ src/ui/recurring.py
@@ -11,14 +11,16 @@ from src.ui.timeseries import *
 #=============================================================================================================
 #TODO: Format using the example "Label Lines with Annotations" from https://plot.ly/python/line-charts/
 #TODO: add name for undefined
-def generateTimeSeries(trackers,timeSeries):
+def generateTimeSeries(trackers,timeSeries,start=0,stop=None):
     #df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv")
     #timeSeries = TimeSeriesData(dataFrame)
     data = []
+    if stop != None:
+        stop = stop + 1  # the [x:y] notation excludes the last index
     for key,name in trackers.items():
         trace = go.Scatter(
-            x=timeSeries.getDates(),
-            y=timeSeries.getSeriesByName(key),
+            x=timeSeries.getDates()[start:stop],
+            y=timeSeries.getSeriesByName(key)[start:stop],
             name = name,
             #line = dict(color = '#17BECF'),
             opacity = 0.8)
@@ -37,7 +39,8 @@ def generateTimeSeries(trackers,timeSeries):
 def generateDatesSlider(dates):
     iMax = len(dates)-1
     slider = dcc.RangeSlider(
-        marks={i:dates[i] for i in range(iMax)},
+        id='dateRange',
+        marks={i:dates[i] for i in range(iMax+1)}, # range excludes the last index
         min=0,
         max=iMax,
         value=[iMax-3,iMax])
@@ -89,13 +92,19 @@ trackersData = getDataPoints()
 layout = html.Div(children=[
     html.H4(children='Recurring Expenses - Work In Pogress'),
     dcc.Graph(id='data', figure=generateTimeSeries(getTrackers(trackers_df), trackersData)),
-    generateDatesSlider(trackersData.getDates()),
-    generateTable(trackers_df),
-    html.Div(id='trackers_table')
+    html.Div(id='slider',className='padded',children=[generateDatesSlider(trackersData.getDates())]),
+    html.Div(id='trackers_table',children=[generateTable(trackers_df)])
 ])
 
 #=============================================================================================================
 # Callbacks
+@app.callback(
+    Output('data', 'figure'),
+    [Input('dateRange', 'value')])
+def applyDateRange(range):
+    figure = generateTimeSeries(getTrackers(trackers_df), trackersData, range[0], range[1])
+    return figure
+
 
 #=============================================================================================================
 
@@ -103,17 +112,20 @@ layout = html.Div(children=[
 #https://community.plot.ly/t/two-graphs-side-by-side/5312
 
 # TODO: Show graph with slider and table with average, min, max, start date, end date and table with alerts
-# TODO: add slider
-# TODO: add table, the table allows toggling the graph and naming the expense.
-# TODO: The new name applies to the monthly report and overview as well
 # DONE: show only name, start, last, count, avg, min, max
-# TODO: align the slider with the graph and add a separation from the table
+# DONE: add slider
+# DONE: align the slider with the graph and add a separation from the table
+# DONE: add table, the table allows toggling the graph and naming the expense.
+# DONE: apply slider to graph
+# DONE: fix last value on slider
+# TODO: apply slider to table
+# TODO: toggle graph lines based on table
 # TODO: when the amount is constant, show it as avg, min & max
 # TODO: rename columns
-# TODO: add section titles
-# TODO: table content dynamic based on slider
 # TODO: make name editable
 # TODO: apply new name to the monthly report and overview as well
-# TODO: toggle graph lines based on table
+# TODO: add section titles
 # TODO: alerts
+# TODO: align slider when resizing window
+
 
